bd997af07a2b80d0459705c830a8782a840f4927
Inna-decsgr <anjgkwl27@gmail.com>
2024-07-29 04:14:30 +0900

    Removed unnecessary options from mongoose.connect().

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,10 +3,7 @@ const dbConfig = require('./db');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbConfig.mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(dbConfig.mongoURI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB 연결 오류:', err);
