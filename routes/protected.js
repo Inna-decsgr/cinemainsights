fceed15dd6bb4e9452a2d8f33f4405b078b36372
Inna-decsgr <anjgkwl27@gmail.com>
2024-07-29 04:13:58 +0900

    Simplify and clean up protected route middleware

--- routes/protected.js
+++ routes/protected.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const { expressjwt: expressJwt } = require('express-jwt');
 const dotenv = require('dotenv');
 
@@ -8,17 +7,21 @@ dotenv.config(); // 환경 변수 로드
 
 const secret = process.env.JWT_SECRET;
 
+const router = express.Router();
+
 // JWT 검증 미들웨어 설정
 router.use(expressJwt({
   secret: secret,
   algorithms: ['HS256'],
   getToken: (req) => req.cookies.token // 쿠키에서 토큰을 가져옴
-}).unless({ path: ['/api/auth/login', '/api/users'] }));
+}).unless({ path: ['/api/auth/login', '/api/users', '/api/movies', '/api/auth/logout'] }));
 
 
 // 보호된 경로
-router.get('/protected', (req, res) => {
+router.get('/', (req, res) => {
   res.send('This is a protected route');
 });
 
+
 module.exports = router;
+
