ab979aa34f09e6c784527ef4fe05feb3d571778e
Inna-decsgr <anjgkwl27@gmail.com>
2024-07-31 23:45:30 +0900

    Add Movie Schema with Reviews and Comments

--- models/Movie.js
+++ models/Movie.js
@@ -1,6 +1,44 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ReviewSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const CommentSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 const MovieSchema = new Schema({
     title: {
         type: String,
@@ -29,10 +67,17 @@ const MovieSchema = new Schema({
     poster_url: { // 영화 포스터 URL 필드 추가
         type: String,
         required: true
-    }
+    },
+    genres: {
+        type: [String],
+        required: true
+    },
+    reviews: [ReviewSchema],
+    comments: [CommentSchema]
 });
 // 각각 다른 컬렉션에 저장되는 모델 정의
 const PopularMovie = mongoose.model('PopularMovie', MovieSchema, 'PopularMovies');
 const LatestMovie = mongoose.model('LatestMovie', MovieSchema, 'LatestMovies');
+const GenreMovie  = mongoose.model('GenreMovie', MovieSchema, 'GenreMovies');
 
-module.exports = { PopularMovie, LatestMovie}; 
+module.exports = { PopularMovie, LatestMovie, GenreMovie}; 
