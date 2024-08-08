b6475e7d9aa794b9a54592a6cb57b430ed46b9e1
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-08 23:18:12 +0900

    Add 'likes' field to User schema for storing user movie preferences

--- models/User.js
+++ models/User.js
@@ -6,8 +6,13 @@ const userSchema = new Schema({
   uid: { type: String, required: true },
   email: { type: String, required: true },
   displayName: { type: String, required: true },
-  photoURL: { type: String }
-}, { collection: 'User' }); // 사용자 정의 컬렉션 이름);
+  photoURL: { type: String },
+  likes: [{
+    movieId: { type: String, required: true },
+    title: { type: String, required: true },
+    imageUrl: { type: String, required: true }
+  }]
+}, { collection: 'User' }); // 사용자 정의 컬렉션 이름
 
 // User 모델 생성
 const User = mongoose.model('User', userSchema);
