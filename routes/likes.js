4f5d1514bfbcd0a67f7c76e0daa2b9825da25e51
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-20 06:16:56 +0900

    Enhance movie liking functionality with additional details

--- routes/likes.js
+++ routes/likes.js
@@ -5,9 +5,9 @@ const User = require('../models/User');
 // 찜하기 기능을 처리하는 POST 요청
 router.post('/', async (req, res) => {
   try {
-    const { uid, movieId, title, imageUrl } = req.body;
+    const { uid, movieId, title, imageUrl, director, cast } = req.body;
 
-    if (!uid || !movieId || !title || !imageUrl) {
+    if (!uid || !movieId || !title || !imageUrl || !director ||!cast) {
       return res.status(400).json({ message: 'Required fields are missing' });
     }
 
@@ -31,7 +31,7 @@ router.post('/', async (req, res) => {
     }
 
     // 새로운 찜 항목 추가
-    user.likes.push({ movieId, title, imageUrl });
+    user.likes.push({ movieId, title, imageUrl, director, cast });
 
     await user.save();
     res.status(200).json({ message: '찜 등록되었습니다' });
