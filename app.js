e085a5639d59dd8fd7b9ebac2b32aa9c375e2bdf
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-22 07:12:39 +0900

    Add genre-based recommendation route and update server configuration

--- app.js
+++ app.js
@@ -54,14 +54,13 @@ app.use('/api/movies', require('./routes/movies'));  // 영화 관련 라우트
 app.use('/api/likes', require('./routes/likes'));  // 찜 관련 라우트
 app.use('/api/bookmarks', require('./routes/bookmarks'));  // 즐겨찾기 관련 라우트
 app.use('/api/recommendations', require('./routes/recommendations'));  // 추천 영화 관련 라우트
+app.use('/api/recommendGenres', require('./routes/recommendGenres'));  // 장르별 추천 영화 관련 라우트
 app.use('/api/reviews', require('./routes/reviews'));  // review 관련 라우트
 
 
 // 서버 시작
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 
 
 
