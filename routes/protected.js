019f2b650699cc102d7be3323902cafca25dd8a4
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-27 23:24:20 +0900

    Updated JWT middleware to retrieve accessToken from cookies

--- routes/protected.js
+++ routes/protected.js
@@ -6,14 +6,17 @@ dotenv.config(); // 환경 변수 로드
 
 
 const secret = process.env.JWT_SECRET;
-
 const router = express.Router();
 
 // JWT 검증 미들웨어 설정
 router.use(expressJwt({
   secret: secret,
   algorithms: ['HS256'],
-  getToken: (req) => req.cookies.token // 쿠키에서 토큰을 가져옴
+
+  getToken: (req) => {
+  console.log('Cookies:', req.cookies); // 쿠키의 내용을 확인
+  return req.cookies.accessToken;
+}  // 쿠키에서 토큰을 가져옴
 }).unless({ path: ['/api/auth/login', '/api/users', '/api/movies/*', '/api/auth/logout', '/api/search'] }));
 
 
